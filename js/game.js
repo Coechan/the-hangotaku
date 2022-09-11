fb666239638816bb6c4057b062a0debf58bce798
Coechan <coe.elena47@gmail.com>
2022-09-11 17:58:39 -0300

    add keyboard and fixed bugs

--- js/game.js
+++ js/game.js
@@ -12,6 +12,8 @@ const animeWord = document.querySelector(".anime");
 const randomAnime = document.querySelector(".random-anime");
 const wrongLetters = document.querySelector(".wrong-letters");
 let chances = 0;
+let correct = 0;
+let gameEnded = false;
 
 // Buttons Selectors
 const saveButton = document.querySelector(".save");
@@ -20,6 +22,7 @@ const startButton = document.querySelector(".start");
 const addWordButton = document.querySelector(".add-word");
 const newGameButton = document.querySelector(".newgame");
 const quitButton = document.querySelector(".quit");
+const charButtons = document.querySelectorAll(".char-button")
 
 //img selector
 const firstTry = document.querySelector(".onetry");
@@ -28,6 +31,7 @@ const thirdTry = document.querySelector(".threetry");
 const fourthTry = document.querySelector(".fourtry");
 const fiveTry = document.querySelector(".fivetry");
 const loser = document.querySelector(".loser");
+const winner = document.querySelector(".winner");
 
 // Anime Array
 let random = '';
@@ -51,16 +55,21 @@ function addAnime(event){
 }
 
 function showAnime(event){
+    gameEnded = false;
     erasedAnime()
     gameScreenShow()
     let indexRandom = Math.floor(Math.random() * animelist.length);
     random= animelist[indexRandom].toUpperCase();
     secretAnime();
     chances = 0;
+    correct = 0;
     hangman();
 }
 
 function showLetters(event){
+    if (gameEnded) {
+        return;
+    }
     let char = document.querySelectorAll(".char");
     if (event.keyCode < 65 || event.keyCode > 90){
         return
@@ -68,7 +77,10 @@ function showLetters(event){
     if (random.includes(event.key.toUpperCase())){
         for(let n = 0 ; n < char.length ; n ++){
             if(char[n].innerHTML == event.key.toUpperCase()){
-                char[n].style.visibility = "";
+                if (char[n].style.visibility === "hidden") {
+                    char[n].style.visibility = "";
+                    correct += 1;
+                }
             }}
     } else {
         if (wrongLettersArray.includes(event.key)) {
@@ -87,6 +99,17 @@ function showLetters(event){
     if (chances == 5){
         for(let g = 0 ; g < char.length ; g ++){
             char[g].style.visibility = "";}
+    }
+    if (random.replace(" ", "").length == correct){
+        firstTry.style.display = "none";
+        secondTry.style.display = "none";
+        thirdTry.style.display = "none"; 
+        fourthTry.style.display = "none";
+        fiveTry.style.display = "none"; 
+        loser.style.display = "none"; 
+        winner.style.display = "flex";
+        gameEnded = true;
+
     }
 }
 
@@ -123,6 +146,7 @@ function hangman(){
         fourthTry.style.display = "none";
         fiveTry.style.display = "none"; 
         loser.style.display = "none"; 
+        winner.style.display = "none";
     }
     if (chances == 1){
         firstTry.style.display = "none";
@@ -146,6 +170,52 @@ function hangman(){
     }
 }
 
+function keyboard(event){
+    if (gameEnded) {
+        return;
+    }
+
+    let pressedChar = event.currentTarget.innerHTML;
+
+    let char = document.querySelectorAll(".char");
+    if (random.includes(pressedChar.toUpperCase())){
+        for(let n = 0 ; n < char.length ; n ++){
+            if(char[n].innerHTML == pressedChar.toUpperCase()){
+                if (char[n].style.visibility === "hidden") {
+                    char[n].style.visibility = "";
+                    correct += 1;
+                }
+            }}
+    } else {
+        if (wrongLettersArray.includes(pressedChar)) {
+            return;
+        } else {
+            let wrongLetters = document.querySelector(".wrong-letters");
+            let wrongList = document.createElement("div");
+            wrongLetters.appendChild(wrongList);
+            wrongList.className = "wrong-list";
+            wrongList.innerHTML = pressedChar.toUpperCase();
+            wrongLettersArray.push(pressedChar);
+            chances += 1;
+            hangman();
+        }
+    }
+    if (chances == 5){
+        for(let g = 0 ; g < char.length ; g ++){
+            char[g].style.visibility = "";}
+    }
+    if (random.replace(" ", "").length == correct){
+        firstTry.style.display = "none";
+        secondTry.style.display = "none";
+        thirdTry.style.display = "none"; 
+        fourthTry.style.display = "none";
+        fiveTry.style.display = "none"; 
+        loser.style.display = "none"; 
+        winner.style.display = "flex";
+        gameEnded = true;
+    }
+}
+
 function homeScreen(event){
     addScreen.style.display = 'none';
     gameScreen.style.display = 'none';
@@ -158,6 +228,7 @@ function gameScreenShow(event){
     gameScreen.style.display = 'flex';
     document.onkeydown = showLetters;
     wrongLettersArray = [];
+    gameEnded = false;
 }
 
 function AddScreenShow(event){
@@ -180,6 +251,9 @@ startButton.onclick = showAnime;
 addWordButton.onclick = AddScreenShow;
 saveButton.onclick = addAnime;
 newGameButton.onclick = showAnime;
+charButtons.forEach( item => {
+    item.onclick = keyboard;
+})
 
 
 
